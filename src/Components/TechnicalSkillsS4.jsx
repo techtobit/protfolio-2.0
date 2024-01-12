4e52645c63cb3ab51733152b1d33122fc0a78afd
codify <nurthern2016@gmail.com>
2024-01-12 14:31:21 +0600

    issuFixed:skill secetions all logs updated

--- src/Components/TechnicalSkillsS4.jsx
+++ src/Components/TechnicalSkillsS4.jsx
@@ -22,16 +22,16 @@ const TechnicalSkillsS4 = () => {
     <SubTitle>Web Development & Coding Is Fun 💻.</SubTitle>
    </div>
    {/* skill-section-one  */}
-   <div className='skill-section-one'>
+   <div className='skill-section-one '>
     <div className="flex justify-center flex-col items-center lg:pb-0 md:pb-0 pb-10">
      <SectionNumBlob> 1 </SectionNumBlob>
      <MainTitle className='capitalize'>FrontEnd Stack</MainTitle>
      {/* <SubTitle>Frontend Stack</SubTitle> */}
     </div>
-    <div className="skills-catagories  grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4 items-center ">
+    <div className="skills-catagories h-screen pt-20 grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4 items-center ">
      <div className="skills-icons grid grid-cols-3 ">
       {
-       skillsIcons.slice(0, 9).map((frontEnd, index) => {
+       skillsIcons.slice(0, 12).map((frontEnd, index) => {
         return (
          <div key={index} className=' skills-list flex flex-col relative  items-center justify-center p-4'>
           <div className="stack-shadow absolute"></div>
@@ -54,16 +54,16 @@ const TechnicalSkillsS4 = () => {
       </div>
      </div>
     </div>
-    <img className='absolute sectionIndicator1 lg:w-[20%] lg:h-[20%] md:w-[22%] md:h-[20%]  lg:bottom-[52%] md:bottom-[35%] lg:left-[26%] md:left-[24%] lg:rotate-[-14deg] md:rotate-[-10deg] lg:block md:block hidden' src={sectionIndicator1} alt="" />
+    <img className='absolute sectionIndicator1 lg:w-[20%] lg:h-[20%] md:w-[22%] md:h-[20%]  lg:bottom-[49.5%] md:bottom-[35%] lg:left-[28%] md:left-[24%] lg:rotate-[-14deg] md:rotate-[-10deg] lg:block md:block hidden' src={sectionIndicator1} alt="" />
    </div>
    {/* skill-section-two  */}
-   <div className="skill-section-two pt-20">
+   <div className="skill-section-two  pt-20">
     <div className="flex justify-center flex-col items-center lg:pb-0 md:pb-0 pb-10">
      <SectionNumBlob> 2 </SectionNumBlob>
      <MainTitle className='capitalize'>Backend Stack</MainTitle>
      {/* <SubTitle>Frontend Stack</SubTitle> */}
     </div>
-    <div className="skills-catagories  grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4 items-center">
+    <div className="skills-catagories h-screen grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4 items-center">
      <div className="catagories-assets lg:flex md:flex justify-start hidden items-center">
       <div className="assets flex items-center">
        <Player
@@ -77,7 +77,7 @@ const TechnicalSkillsS4 = () => {
      </div>
      <div className="skills-icons grid grid-cols-3 ">
       {
-       skillsIcons.slice(9, 15).map((backEnd, index) => {
+       skillsIcons.slice(12, 21).map((backEnd, index) => {
         return (
          <div key={index} className=' skills-list flex flex-col relative  items-center justify-center p-4'>
           <div className="stack-shadow absolute"></div>
@@ -89,11 +89,11 @@ const TechnicalSkillsS4 = () => {
       }
      </div>
     </div>
-    <img className='absolute sectionIndicator2 lg:w-[24%] lg:h-[20%] md:w-[34%] md:h-[22%]  lg:bottom-[22%] md:bottom-[12%] lg:left-[50%] md:left-[50%]  lg:block md:block hidden' src={sectionIndicator2} alt="" />
+    <img className='absolute sectionIndicator2 lg:w-[26%] lg:h-[26%] md:w-[34%] md:h-[22%]  lg:bottom-[14%] md:bottom-[12%] lg:left-[49.3%] md:left-[50%]  lg:block md:block hidden' src={sectionIndicator2} alt="" />
    </div>
 
    {/* skill-section-three  */}
-   <div className="skill-section-three pt-20 ">
+   <div className="skill-section-three h-screen pt-20 ">
     <div className="flex justify-center flex-col items-center lg:pb-0 md:pb-0 pb-10">
      <SectionNumBlob> 3 </SectionNumBlob>
      <MainTitle className='capitalize'>Technical Tools</MainTitle>
@@ -102,7 +102,7 @@ const TechnicalSkillsS4 = () => {
     <div className="skills-catagories  grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-4 items-center">
      <div className="skills-icons grid grid-cols-3 ">
       {
-       skillsIcons.slice(15, 24).map((techTools, index) => {
+       skillsIcons.slice(21, 31).map((techTools, index) => {
         return (
          <div key={index} className=' skills-list flex flex-col relative  items-center justify-center p-4'>
           <div className="stack-shadow absolute"></div>
