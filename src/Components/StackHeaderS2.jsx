aa5f4b608317e068d63c72a1a6820934ebe0d45e
techtobit <nurthern2016@gmail.com>
2024-12-09 17:28:49 +0600

    stackhader: mobile responsive

--- src/Components/StackHeaderS2.jsx
+++ src/Components/StackHeaderS2.jsx
@@ -6,18 +6,18 @@ import '../Style/StackHeaderS2.scss'
 const StackHeaderS2 = () => {
   return (
     <>
-      <div className="stack-header lg:flex hidden justify-center absolute bottom-[15%] ">
+      <div className="stack-header flex  justify-center absolute lg:bottom-[15%] bottom-[8%] ">
         <div className="stack-shadow absolute"></div>
         <div className="stack-shadow-two absolute"></div>
-        <p className='stack-card-title absolute text-md font-bold underline'>Open to work as</p>
+        <p className='stack-card-title z-40 absolute top-[90%] lg:left-[24%] text-md font-bold underline'>Open to work as</p>
         <div className="stack-list absolute z-30 grid grid-cols-3 rounded-2xl justify-items-center items-center content-center drop-shadow-xl">
-          <div className="stack flex flex-col items-center lg:pb-0 pb-6 px-6 border-blue-50 lg:border-l-[0.4px] ">
+          <div className="stack flex flex-col items-center lg:pb-0 lg:px-6 border-blue-50 lg:border-l-[0.4px] ">
             <h3>Full Time</h3>
           </div>
-          <div className="stack flex flex-col items-center  lg:pb-0 pb-6  px-6 border-blue-50 lg:border-l-[0.4px]">
+          <div className="stack flex flex-col items-center  lg:pb-0 lg:px-6 border-blue-50 lg:border-l-[0.4px]">
             <h3>Part Time</h3>
           </div>
-          <div className="stack flex flex-col items-center lg:pb-0 pb-6 px-6 border-blue-50 lg:border-l-[0.4px]">
+          <div className="stack flex flex-col items-center lg:pb-0 lg:px-6 border-blue-50 lg:border-l-[0.4px]">
             <h3>Contractual</h3>
           </div>
         </div>
