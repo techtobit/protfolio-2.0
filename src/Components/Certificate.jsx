dd10ab2629ebe2ddf5b863b2fd074bb2d8f8fc65
techtobit <nurthern2016@gmail.com>
2025-01-06 15:23:15 +0600

    phitron ctr added

--- src/Components/Certificate.jsx
+++ src/Components/Certificate.jsx
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import html_css from '../Assets/Certificate/HTML CSS JS .jpg'
+import CSE_Fundamentals from '../Assets/Certificate/CSE Fundamentals.jpg'
 import full_stack from '../Assets/Certificate/Full Stack Web Development.jpg'
 import effective from '../Assets/Certificate/Effective.jpg';
 import intern from '../Assets/Certificate/intership.jpg';
@@ -51,9 +52,9 @@ const Certificate = () => {
 
 	const timelineData = [
 		{
-			year: "2021",
-			courseName: "Html CSS and Javascirpt",
-			image: `${html_css}`,
+			year: "2023",
+			courseName: "CSE Fundamentals Professional Certificate",
+			image: `${CSE_Fundamentals}`,
 		},
 		{
 			year: "2022",
@@ -61,14 +62,9 @@ const Certificate = () => {
 			image: `${full_stack}`,
 		},
 		{
-			year: "2022",
-			courseName: "Effect Listening",
-			image: `${effective}`,
-		},
-		{
-			year: "2022",
-			courseName: "Agile Team and Scrum Lead",
-			image: `${algile}`,
+			year: "2021",
+			courseName: "Html CSS and Javascirpt",
+			image: `${html_css}`,
 		},
 		{
 			year: "2023",
@@ -77,8 +73,18 @@ const Certificate = () => {
 		},
 		{
 			year: "2022",
+			courseName: "Agile Team and Scrum Lead",
+			image: `${algile}`,
+		},
+		{
+			year: "2022",
 			courseName: "CSS",
 			image: `${hackerRankCss}`,
+		},
+		{
+			year: "2022",
+			courseName: "Effect Listening",
+			image: `${effective}`,
 		},
 		// Add more data here
 	];
